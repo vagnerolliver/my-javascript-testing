2d11191e9262b03491411ec232a6d2822c865453
Vágner Oliveira <vagner.oliveira@zenvia.com>
2020-11-16 20:32:17 -0300

    structure(layers) projects done

---
+++ tdd-project-02/services/carService.js
@@ -0,0 +1,14 @@
+
+const BaseRepository = require('./../repository/base/baseRepository')
+
+class CarService {
+    constructor({ cars }) {
+        this.carRepository = new BaseRepository({ file: cars })
+    }
+
+    test() {
+        return this.carRepository.find()
+    }
+}
+
+module.exports = CarService
